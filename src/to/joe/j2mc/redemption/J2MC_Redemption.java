0f2283f6d10e4c954403b1978968969f2f93f3dc
Ivan Panarusky <eviltechie@joe.to>
2012-09-30 03:03:54 -0400

    forgot to register

--- src/to/joe/j2mc/redemption/J2MC_Redemption.java
+++ src/to/joe/j2mc/redemption/J2MC_Redemption.java
@@ -1,13 +1,14 @@
 package to.joe.j2mc.redemption;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class J2MC_Redemption extends JavaPlugin implements Listener {
+import to.joe.j2mc.redemption.command.RedeemCommand;
+
+public class J2MC_Redemption extends JavaPlugin {
     
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(this, this);
+        this.getCommand("redeem").setExecutor(new RedeemCommand(this));
     }
     
 }
